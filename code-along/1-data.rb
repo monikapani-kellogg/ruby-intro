1c7665f44551122eab473d76a99a557ff8343f0a
Monika Pani <monika.pani@kellogg.northwestern.edu>
2023-04-20 00:00:35 +0000

    Ruby 101

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -6,14 +6,43 @@
 
 # Numbers
 
+puts 3
+puts 2
+puts 3+2
+puts 3-2
+puts 3*2
+puts 3/2
+puts 3/2.0
+puts 7%4
+puts 5*2-2
+puts 5*(2-2)
+
 # Perform simple math with numbers
 
 # Strings
 
+puts "Hello World"
+
 # Combine strings together
+
+puts "Hello "+"World"
+puts "Tacos are delish"
+puts "Tacos"*3 # This is special to Ruby - not many languages can do this
+
 
 # Variables
 
+food = "Tacos"
+number = 3
+puts food, number
+puts food*number
+
 # Combine strings and variables
 
+greeting = "Heyyyyy"
+welcome_message= "#{greeting}, world"
+
 # String manipulation
+
+puts welcome_message.upcase
+puts welcome_message.reverse
